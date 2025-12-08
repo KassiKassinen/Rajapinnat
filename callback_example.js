4da74275b20709490924d80d74b4c0148d7d197a
KassiKassinen <aleksisakari.makinen@gmail.com>
2025-12-08 17:23:53 +0200

    tehtävä 15

---
+++ callback_example.js
@@ -0,0 +1,27 @@
+//15.1
+
+/*setTimeout(doSomething,2000);
+
+function doSomething(){
+    console.log("Demonstrating the callbacks");
+}
+console.log("The application is started");
+*/
+
+
+//15.2
+/*setTimeout(function(){
+    console.log("Demonstrating the callbacks");
+},2000);
+
+
+console.log("The application is started");
+*/
+
+//15.3
+setTimeout(()=>{
+    console.log("Demonstrating the callbacks");
+},2000);
+
+
+console.log("The application is started");
